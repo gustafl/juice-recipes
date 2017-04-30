460faf140b79ea620c79c6ed4ce7279a2dea9a61
Gustaf Liljegren <gustafl@bahnhof.se>
2017-04-30 14:33:56 +0200

    Added logging

--- request.py
+++ request.py
@@ -1,4 +1,5 @@
 #pylint: disable-all
+import datetime
 import hashlib
 import os
 import re
@@ -9,11 +10,10 @@ from lxml import etree
 
 ROOTS_FILE = "roots.xml"
 CACHE_DIR = "cache"
+LOG_DIR = "log"
 OUTPUT_DIR = "output"
 REQUEST_DELAY = 3
 MAX_LEVEL = 2
-
-url = "https://pylint.readthedocs.io/en/latest/faq.html"
 
 def get_unique_filename(text):
     """Get a unique alpha-numeric filename using the first few characters from a SHA1 hash of the input string."""
@@ -43,7 +43,7 @@ def save_cache_file(filename, text, encoding):
     with open(path, mode="wt", encoding=encoding) as f:
         f.write(text)
 
-def load_cached_file(url):
+def load_cached_file(url, log_path):
     """Get the BeautifulSoup object from the cache, or return None."""
     # Make sure the cache directory exists
     if os.path.exists(CACHE_DIR):
@@ -64,29 +64,33 @@ def load_cached_file(url):
                         else:
                             raise ValueError("The cached file is empty:\n  " + url)
                     except IOError:
-                        print("IOError when reading cached resource:\n  " + url)
+                        message = "IOError when reading cached resource:\n  " + url
+                        print(message)
+                        log(log_path, message)
                     except UnicodeDecodeError:
-                        print("UnicodeDecodeError when reading cached resource:\n  " + url)
+                        message = "UnicodeDecodeError when reading cached resource:\n  " + url
+                        print(message)
+                        log(log_path, message)
                     except ValueError as error:
                         print(str(error))
-                    
+                        log(log_path, str(error))
     return None
 
-def get_soup(url):
+def get_soup(url, log_path):
     """Get BeautifulSoup object from cache or web."""
     cached = False
-    soup = load_cached_file(url)
+    soup = load_cached_file(url, log_path)
     if soup:
         cached = True
     else:
         # If no cached soup was found, download the page
         with urllib.request.urlopen(url) as response:
-            html = response.read()
-            soup = BeautifulSoup(html, "lxml")
-            encoding = get_encoding(soup)
+            encoding = response.headers.get_content_charset()
+            content = response.read().decode(encoding, "replace")
+            soup = BeautifulSoup(content, "lxml")
             filename = get_unique_filename(url)
             save_cache_file(filename, soup.prettify(), encoding)
-    return soup, cached
+    return soup
 
 def is_recipe_link(tag):
     """Return true if the input tag is an <a> link to a recipe."""
@@ -138,19 +142,31 @@ def save_recipe(url, soup):
         recipe = etree.SubElement(root, "recipe")
         recipe.set("source", url)
         for i in ingredients:
-            ingredient = etree.SubElement(recipe, "ingredient")
-            ingredient.text = i.string.strip()
+            if type(i.string) == "string":
+                ingredient = etree.SubElement(recipe, "ingredient")
+                ingredient.text = i.string.strip()
         document.write(path, encoding="utf-8", xml_declaration=True, pretty_print=True)
 
-def handle_url(url, level):
+def get_log_path():
+    # Create log directory if missing
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+    # Get path to log file
+    filename = datetime.datetime.today().strftime("%y%m%d_%H%M%S.txt")
+    path = os.path.join(".", LOG_DIR)
+    path = os.path.join(path, filename)
+    return path
+
+def log(path, url):
+    # Write to log
+    with open(path, mode="at", encoding="utf-8") as f:
+        f.write(url + "\n")
+
+def handle_url(url, level, log_path):
     level += 1
     if level <= MAX_LEVEL:
-        soup, cached = get_soup(url)
-        print(url, "cached=" + str(cached))
-        if not cached:
-            encoding = get_encoding(soup)
-            filename = get_unique_filename(url)
-            save_cache_file(filename, soup.prettify(), encoding)
+        log(log_path, url)
+        soup = get_soup(url, log_path)
         if is_recipe_page(soup):
             save_recipe(url, soup)
         else:
@@ -159,11 +175,12 @@ def handle_url(url, level):
                 url = tag.get("href")
                 if url:
                     time.sleep(REQUEST_DELAY)
-                    handle_url(url, level)
+                    handle_url(url, level, log_path)
 
+log_path = get_log_path()
 document = etree.parse(ROOTS_FILE)
 root = document.getroot()
 for child in root:
     url = child.attrib.get("url")
     if url and len(url.strip()) > 0:
-        handle_url(url, 0)
+        handle_url(url, 0, log_path)
